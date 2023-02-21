66a284fe2fe3a60df8f551d8b5cafd2a7f35dae5
koen-db <koen.de.bruyne@howest.be>
2023-02-21 13:35:37 +0100

    Add extra comparisons

--- H3 - selectie/compare.py
+++ H3 - selectie/compare.py
@@ -1,7 +1,9 @@
-x = input("Geef een getal voor x: ")
-y = input("Geef een getal voor y: ")
+x = int(input("Geef een getal voor x: "))
+y = int(input("Geef een getal voor y: "))
 
+if x < y:
+    print("x is kleiner dan y")
+if x > y:
+    print("x is groter dan y")
 if x == y:
     print("x is gelijk aan y")
-else:
-    print("x is niet gelijk aan y")
