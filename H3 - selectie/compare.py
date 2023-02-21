0a970b16212377bd84576433541035f665c32aa8
koen-db <koen.de.bruyne@howest.be>
2023-02-21 13:50:23 +0100

    Introduce the 'or' statement

--- H3 - selectie/compare.py
+++ H3 - selectie/compare.py
@@ -1,9 +1,7 @@
 x = int(input("Geef een getal voor x: "))
 y = int(input("Geef een getal voor y: "))
 
-if x < y:
-    print("x is kleiner dan y")
-elif x > y:
-        print("x is groter dan y")
+if x < y or x > y:
+        print("x is kleiner of groter dan y")
 else:
     print("x is gelijk aan y")
