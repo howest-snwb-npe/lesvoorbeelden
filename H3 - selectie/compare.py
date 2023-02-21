06c0973fd287d7701e9b1d751a96b9cab01fd3bb
koen-db <koen.de.bruyne@howest.be>
2023-02-21 13:41:24 +0100

    Add multiple 'else' statements

--- H3 - selectie/compare.py
+++ H3 - selectie/compare.py
@@ -3,7 +3,9 @@ y = int(input("Geef een getal voor y: "))
 
 if x < y:
     print("x is kleiner dan y")
-if x > y:
-    print("x is groter dan y")
-if x == y:
-    print("x is gelijk aan y")
+else:
+    if x > y:
+        print("x is groter dan y")
+    else:
+        if x == y:
+            print("x is gelijk aan y")
