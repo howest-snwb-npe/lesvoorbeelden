b5f61b4fe8e8e4d7ce8c96de7daaf8aa5bc00c6f
koen-db <koen.de.bruyne@howest.be>
2023-02-21 13:43:50 +0100

    Introduce the 'elif' statement

--- H3 - selectie/compare.py
+++ H3 - selectie/compare.py
@@ -3,8 +3,7 @@ y = int(input("Geef een getal voor y: "))
 
 if x < y:
     print("x is kleiner dan y")
-else:
-    if x > y:
+elif x > y:
         print("x is groter dan y")
-    else:
-        print("x is gelijk aan y")
+else:
+    print("x is gelijk aan y")
